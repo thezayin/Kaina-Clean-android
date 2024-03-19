f8f19fb3042f5722d5919426f8461dd0892e5943
thezayin <zainshahidbuttt@gmail.com>
2024-03-19 11:06:26 +0500

    Implement service module

--- app/src/main/java/com/thezayin/kainaclean/services/domain/usecases/ServiceUseCases.kt
+++ app/src/main/java/com/thezayin/kainaclean/services/domain/usecases/ServiceUseCases.kt
@@ -1,7 +1,5 @@
 package com.thezayin.kainaclean.services.domain.usecases
 
-import com.thezayin.kainaclean.services.domain.usecases.ServicesOptionCase
-
 data class ServiceUseCases(
     val serviceOptionsCase: ServicesOptionCase,
 )
